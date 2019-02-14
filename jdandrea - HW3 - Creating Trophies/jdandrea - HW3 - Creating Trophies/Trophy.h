ca38468a0f6d54464db844181f5fac1489621ac1
Jdandrea40 <jdandrea40@yahoo.com>
2019-02-14 10:22:03 -0700

    HW 3 - Trophy Start
    - Copied all code from MOODLE over

---
+++ jdandrea - HW3 - Creating Trophies/jdandrea - HW3 - Creating Trophies/Trophy.h
@@ -0,0 +1,10 @@
+#pragma once
+// TODO: Guard the header
+
+// TODO: include the libraries this class will need
+
+enum Color;		// TODO: create the color values
+
+class Trophy;	// TODO: complete the Trophy class
+
+// TODO: end the guard
