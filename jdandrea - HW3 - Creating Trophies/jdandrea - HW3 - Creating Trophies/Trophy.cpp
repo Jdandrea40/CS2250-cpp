7fdeb68be62e4d0b903338115a0c3cb247cd2d51
Jdandrea40 <jdandrea40@yahoo.com>
2019-02-15 12:50:58 -0700

    Trophy 2
    - started, define header class functions and private data
    -switch statement for trophy Color type

--- jdandrea - HW3 - Creating Trophies/jdandrea - HW3 - Creating Trophies/Trophy.cpp
+++ jdandrea - HW3 - Creating Trophies/jdandrea - HW3 - Creating Trophies/Trophy.cpp
@@ -1,1 +1,60 @@
 // TODO: implement the Trophy class
+#include "Trophy.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Trophy::Trophy(const string& name, int& level, Color& color)
+{
+	switch (color)
+	{
+	case(GOLD):
+		{
+		m_name = "Super Mega Trophy";
+		m_level = 9001;
+			break;
+		}
+	case(SILVER):
+		{
+		m_name = "If you aint First, you're Last!";
+		m_level = 69;
+			break;
+		}
+	case(BRONZE):
+	{
+		m_name = "I mean, you tried...right?";
+		m_level = 0;
+		break;
+	}
+	}
+}
+string Trophy::getName() 
+{
+	string name;
+	return m_name;
+}
+int Trophy::getLevel()
+{
+	int level;
+	return m_level;
+}
+Color Trophy::getColor()
+{
+	Color color;
+
+	return m_color;
+}
+void Trophy::setName(const string& name)
+{
+	m_name = name;
+}
+void Trophy::setLevel(int& level)
+{
+	m_level = level;
+}
+void Trophy::setColor(const Color& color)
+{
+	m_color = color;
+}
+
--- jdandrea - HW3 - Creating Trophies/jdandrea - HW3 - Creating Trophies/Trophy.h
+++ jdandrea - HW3 - Creating Trophies/jdandrea - HW3 - Creating Trophies/Trophy.h
@@ -1,10 +1,31 @@
-#pragma once
-// TODO: Guard the header
+#ifndef WEAPON_H  // TODO: Guard the header
+#define WEAPON_H
 
 // TODO: include the libraries this class will need
 
-enum Color;		// TODO: create the color values
+enum Color
+{
+	GOLD,
+	SILVER,
+	BRONZE
+};		// TODO: create the color values
 
-class Trophy;	// TODO: complete the Trophy class
+class Trophy
+{
+public:
+	Trophy(const string& name, int& level, Color& color);
+	string getName();
+	int getLevel();
+	Color getColor();
+	void setName(const string& name);
+	void setLevel(int& level);
+	void setColor(const Color& color);
+private:
+	string m_name;
+	Color m_color;
+	int m_level;
+
+};	// TODO: complete the Trophy class
 
 // TODO: end the guard
+#endif // ! WEAPON_H
